ae763226f5f7cb3dc0dddd81e7d9de3d00ecd50f
Fernando Oliveto <fernando.oliveto@aon.com>
2020-03-18 19:19:43 -0300

    Actualizado id

--- detail.php
+++ detail.php
@@ -11,7 +11,8 @@
 	$unit = $_REQUEST['unit'];
 	
 	// Credenciales
-	MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398');
+	//MercadoPago\SDK::setAccessToken('APP_USR-6317427424180639-090914-5c508e1b02a34fcce879a999574cf5c9-469485398');
+	MercadoPago\SDK::setAccessToken('TEST-8196777983571350-031822-67512cd23d704fe6dfb8670cdcd5c825-469485398');
 	//MercadoPago\SDK::setIntegratorId('INTEGRATOR_ID');
   
 	// Datos del item
